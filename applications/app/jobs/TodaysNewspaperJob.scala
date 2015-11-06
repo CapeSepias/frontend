8665c00b6cfc4fc8511d0bcfbf61086aa23e8413
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-11-06 16:21:39 +0000

    Added more sections

--- applications/app/jobs/TodaysNewspaperJob.scala
+++ applications/app/jobs/TodaysNewspaperJob.scala
@@ -9,29 +9,38 @@ import implicits.Dates
 import layout.{CollectionEssentials, FaciaContainer}
 import org.joda.time.{DateTime, DateTimeZone}
 import services.{CollectionConfigWithId, FaciaContentConvert}
-import slices.{Fixed, FixedContainers}
+import slices.{TTT, ContainerDefinition, Fixed, FixedContainers}
 
 import scala.concurrent.Future
 
+case class FrontPageBookSections(id: String, displayName: String)
 
 object TodaysNewspaperJob extends ExecutionContexts with Dates with Logging {
   protected val todaysPaper = AkkaAgent[Set[FaciaContainer]](Set.empty)
 
+  val bookSections = List(
+    FrontPageBookSections("theguardian/mainsection/topstories", "Front page"),
+    FrontPageBookSections("theguardian/mainsection/uknews", "UK news"),
+    FrontPageBookSections("theguardian/mainsection/eyewitness", "Eyewitness"),
+    FrontPageBookSections("theguardian/mainsection/international", "International"),
+    FrontPageBookSections("theguardian/mainsection/financial3", "Financial") //TODO use the webTitle
+  )
+
   def fetchTodaysPaper {
     val today = DateTime.now(DateTimeZone.UTC).withTimeAtStartOfDay()
 
-    val mainSection = "theguardian/mainsection/topstories" //todo add other main sections
-    val mainSectionName = "Front page"
+    bookSections.foreach { bookSection =>
+
+      val item = LiveContentApi.item(bookSection.id).useDate("newspaper-edition").showFields("all").showElements("all").pageSize(20).fromDate(today) //todo restrict show-fields?
+      val resultsF: Future[FaciaContainer] = LiveContentApi.getResponse(item).map { resp =>
+          val content = getResultsOrderByNewspaperNumber(resp.results).map(c => FaciaContentConvert.frontendContentToFaciaContent(Content(c)))
+          bookSectionContainer(bookSection.id, bookSection.displayName, content, bookSections.indexOf(bookSection))
+        }
 
 
-    val item = LiveContentApi.item(mainSection).useDate("newspaper-edition").showFields("all").showElements("all").pageSize(20).fromDate(today) //todo restrict show-fields?
-    val resultsF: Future[FaciaContainer] = LiveContentApi.getResponse(item).map { resp =>
-      val content = getResultsOrderByNewspaperNumber(resp.results).map( c => FaciaContentConvert.frontendContentToFaciaContent(Content(c)))
-      bookSectionContainer(mainSection, mainSectionName, content)
-    }
-
-    resultsF.map { results =>
-      todaysPaper.alter { _ + results }
+      resultsF.map { results =>
+        todaysPaper.alter { _ + results }
+      }
     }
   }
 
@@ -43,13 +52,15 @@ object TodaysNewspaperJob extends ExecutionContexts with Dates with Logging {
     pageNumberToContent.sortBy(_._1).map(_._2)
   }
 
-  private def bookSectionContainer(dataId: String, displayName: String, trails: Seq[FaciaContent]): FaciaContainer = {
-
-    //todo define with K & M
-    val containerDefinition = FixedContainers.fixedMediumFastXII
+  private def bookSectionContainer(dataId: String, displayName: String, trails: Seq[FaciaContent], index: Int): FaciaContainer = {
+    val containerDefinition = trails.length match {
+      case 1 => FixedContainers.fixedSmallSlowI
+      case 2 => FixedContainers.fixedSmallSlowII
+      case 3 => ContainerDefinition.ofSlices(TTT)
+      case _ => FixedContainers.fixedMediumFastXII }
 
     FaciaContainer(
-      0,
+      index,
       Fixed(containerDefinition),
       CollectionConfigWithId(dataId, CollectionConfig.empty.copy(displayName = Some(displayName))),
       CollectionEssentials(trails, Nil, Some(displayName), Some(dataId), None, None)
